c41f21eaf400a9f54f85d0e9c89914958052f82e
rusolm <leandromarquesp88@gmail.com>
2018-11-24 10:44:28 +0100

    get category titles

--- scrape.js
+++ scrape.js
@@ -1,19 +1,55 @@
-const request = require('request');
+const rp = require('request-promise');
 const cheerio = require('cheerio');
-console.log('hlfaslfa');
 
-request('http://codedemos.com/sampleblog'),
-  (error, response, html) => {
-    console.log(error, response, html);
+const options = {
+  url: 'https://canopy.co/shop/gifts',
+  transform: function(body) {
+    return cheerio.load(body);
+  }
+};
 
-    if (!error && response.statusCode == 200) {
-      const $ = cheerio.load(html);
-      // const category = $('.CollectionGrid-tileImage');
-      // console.log('hola');
+rp(options)
+  .then(data => {
+    data('.CollectionGrid-tileName').each((i, el) => {
+      const title = cheerio
+        .load(el)
+        .text()
+        .trim();
 
-      console.log($);
+      console.log(title);
+    });
+  })
+  .catch(err => console.log('ERROR', err));
 
-      // console.log(category.html());
-    }
-    console.log(error);
-  };
+// rp(options)
+//   .then(data => {
+//     data('.CollectionGrid-tile').each((i, el) => {
+//       const title = cheerio
+//         .load(el)
+//         .find('.CollectionGrid-tileName')
+//         .text()
+//         .trim();
+
+//       const link = cheerio.load(el).attr('href');
+
+//       console.log(title);
+//       console.log(link);
+//     });
+//   })
+//   .catch(err => console.log('ERROR', err));
+
+// request('http://codedemos.com/sampleblog'),
+//   (error, response, html) => {
+//     console.log(error, response, html);
+
+//     if (!error && response.statusCode == 200) {
+//       const $ = cheerio.load(html);
+//       // const category = $('.CollectionGrid-tileImage');
+//       // console.log('hola');
+
+//       console.log($);
+
+//       // console.log(category.html());
+//     }
+//     console.log(error);
+//   };
