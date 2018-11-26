f3c89c2ab183be03b5f674f3438f77bf5a499438
Amy Kirasack <amykirasack@gmail.com>
2018-11-26 17:23:45 +0100

    feat(models): Work on category model

--- models/categoryModel.js
+++ models/categoryModel.js
@@ -4,9 +4,29 @@ const db = require('./../schemas');
 
 categoryModel.getAll = async () => {
   const categories = await db.Category.findAll();
-  console.log('here are categories', categories);
+  // console.log('ooo', categories.get('category_name'));
+  console.log('here are all categories', categories);
+  const allCats = categories.map(cat => cat.dataValues);
+  console.log('meow', allCats);
+};
+
+categoryModel.getAllItems = async category_id => {
+  const items = await db.ItemCategory.findAll({
+    where: { category_id },
+    include: [
+      {
+        model: db.Item,
+        as: 'item'
+      }
+    ]
+    // attributes: ['item_id']
+  });
+  // console.log('items here', items.dataValues);
+  const itemIDs = items.map(item => item.dataValues);
+  console.log('aaa', itemIDs);
 };
 
 categoryModel.getAll();
+// categoryModel.getAllItems(1);
 
 module.exports = categoryModel;
