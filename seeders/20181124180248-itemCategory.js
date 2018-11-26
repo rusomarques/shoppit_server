b1cde248fc0cf7ce15c07385ff8ec80436be60d5
Amy Kirasack <amykirasack@gmail.com>
2018-11-26 17:34:01 +0100

    feat(mocks): Edit mock data

--- seeders/20181124180248-itemCategory.js
+++ seeders/20181124180248-itemCategory.js
@@ -11,8 +11,20 @@ module.exports = {
         updatedAt: new Date()
       },
       {
-        item_id: data.items[0].item_id,
-        category_id: data.categories[1].category_id,
+        item_id: data.items[1].item_id,
+        category_id: data.categories[0].category_id,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        item_id: data.items[2].item_id,
+        category_id: data.categories[3].category_id,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        item_id: data.items[3].item_id,
+        category_id: data.categories[2].category_id,
         createdAt: new Date(),
         updatedAt: new Date()
       }
