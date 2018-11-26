e52e7cac5b102cd019a98eef6dde36313ad5a835
rusolm <leandromarquesp88@gmail.com>
2018-11-26 19:44:53 +0100

    feat(item controller): save items from certain category to db

--- controllers/itemsController.js
+++ controllers/itemsController.js
@@ -1,6 +1,37 @@
 /* eslint-disable no-console */
+const itemModel = require('./../models/itemModel');
+const scrape = require('./../scrape/get-items');
+
 const itemsController = {};
 // const Item = require('./../models/itemModel');
+
+itemsController.setItemsfromCategory = async (
+  mainCategory,
+  category,
+  limit,
+  offset
+) => {
+  try {
+    const data = await scrape.getCategoryItems(
+      mainCategory,
+      category,
+      limit,
+      offset
+    );
+    const bulk = data.items.map(item => ({
+      item_id: item.id,
+      item_name: item.name,
+      img_url: item.image,
+      amazon_url: item.amazon_link,
+      price: item.price,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }));
+    await itemModel.setItemsfromCategory(bulk);
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 itemsController.getRecommended = async (req, res) => {
   // do stuff;
