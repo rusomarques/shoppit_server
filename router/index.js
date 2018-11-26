b7bbd174506a20afe39d3b5050fea2487449c94b
Amy Kirasack <amykirasack@gmail.com>
2018-11-26 13:00:01 +0100

    refactor(controllers): Rename controllers and paths

--- router/index.js
+++ router/index.js
@@ -1,36 +1,56 @@
 const express = require('express');
 const router = express.Router();
-const users = require('./../controllers/usersController');
-const categories = require('./../controllers/categoriesController');
-const items = require('./../controllers/itemsControllers');
+const usersController = require('./../controllers/usersController');
+const categoriesController = require('./../controllers/categoriesController');
+const itemsController = require('./../controllers/itemsController');
 // to authenticate user identity
 const authMiddleware = require('./../middlewares/authMiddleware');
 
-// me (users) controllers
+// (User) me controllers
 router
   // get user info including liked categories
-  .get('/me', authMiddleware, users.getOwnInfo)
+  .get('/me', authMiddleware, usersController.getOwnInfo)
   // include friends' liked categories?
-  .get('/me/friends', authMiddleware, users.getFriends)
-  .put('/me/categories/:category_id', authMiddleware, users.addCategory)
-  .delete('/me/categories/:category_id', authMiddleware, users.removeCategory);
+  .get('/me/friends', authMiddleware, usersController.getFriends)
+  .put(
+    '/me/categories/:category_id',
+    authMiddleware,
+    usersController.addCategory
+  )
+  .delete(
+    '/me/categories/:category_id',
+    authMiddleware,
+    usersController.removeCategory
+  );
 // .post('/me/friends', user.addFriend)
 // .delete('/me/friends', user.deleteFriend)
 
-// users controllers
-router.get('/users/:user_id/items', authMiddleware, users.getLikedItems);
+// User controllers
+router.get(
+  '/users/:user_id/items',
+  authMiddleware,
+  usersController.getLikedItems
+);
 
 // Item controllers
 router
   // get the array of recommended items for authenticated user
-  .get('/items/recommended', authMiddleware, items.getRecommended)
-  .put('/items/:item_id/like/:value', authMiddleware, items.setAffinity)
-  .delete('/items/:item_id/like/', authMiddleware, items.removeAffinity);
+  .get('/items/recommended', authMiddleware, itemsController.getRecommended)
+  .put(
+    '/items/:item_id/like/:value',
+    authMiddleware,
+    itemsController.setAffinity
+  )
+  .delete(
+    '/items/:item_id/like/',
+    authMiddleware,
+    itemsController.removeAffinity
+  );
 
 // Category controllers
 router
   // append category_id and send auth token through headers?
-  .get('/categories', categories.getAll)
-  .get('/categories/:category_id/items', categories.getAllItems);
+  .get('/categories', categoriesController.getAll)
+  .get('/categories/:category_id/items', categoriesController.getAllItems);
 
 module.exports = router;
