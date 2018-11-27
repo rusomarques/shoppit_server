a373b4e834e08079ca6523dc061d7b5c92580597
rusolm <leandromarquesp88@gmail.com>
2018-11-27 19:31:02 +0100

    feat(item controller): set affinity

--- controllers/itemsController.js
+++ controllers/itemsController.js
@@ -33,17 +33,34 @@ itemsController.setItemsfromCategory = async (
 
 itemsController.getRecommended = async (req, res) => {
   try {
+    // user id may not be in req.body
     const { user_id } = req.body;
     const result = {
       user_id
     };
     result.items = await Item.getRecommended(user_id);
-    res.send(result).status(200);
+    res.send(result).sendStatus(200);
   } catch (e) {
     console.log(e); // eslint-disable-line no-console
   }
 };
 
-itemsController.setAffinity = async () => {};
+itemsController.setAffinity = async (req, res) => {
+  try {
+    // user id may not be in req.body
+    const { user_id } = req.body;
+
+    const item_id = parseInt(req.params.item_id);
+    let affinity = req.params.value;
+    if (affinity === 'true') affinity = true;
+    else if (affinity === 'false') affinity = false;
+    else throw new Error('affinity must be true or false');
+    await Item.setAffinity(user_id, item_id, affinity);
+    res.sendStatus(201);
+  } catch (e) {
+    console.log(e); // eslint-disable-line no-console
+    res.sendStatus(400);
+  }
+};
 
 module.exports = itemsController;
