9e2a6f834f05f7c73ecec7cce47992354f51956c
rusolm <leandromarquesp88@gmail.com>
2018-11-27 14:41:36 +0100

    feat(item model): set affinity - update if affinity exists and create if does not

--- models/itemModel.js
+++ models/itemModel.js
@@ -13,11 +13,27 @@ itemModel.setItemsfromCategory = async bulk => {
 itemModel.getRecommended = async () => {};
 
 itemModel.setAffinity = async (user_id, item_id, affinity) => {
-  await db.UserItem.create({
-    user_id,
-    item_id,
-    affinity
+  const user = await db.User.findOne({
+    where: { user_id }
   });
+  const item = await user.getItem({ where: { item_id } });
+  if (item.length) {
+    await db.UserItem.update(
+      { affinity },
+      {
+        where: {
+          user_id,
+          item_id
+        }
+      }
+    );
+  } else {
+    await db.UserItem.create({
+      user_id,
+      item_id,
+      affinity
+    });
+  }
 };
 
 itemModel.removeAffinity = async () => {};
