d8325098340d727b57cb4fee7a148407c5a3e5fe
Amy Kirasack <amykirasack@gmail.com>
2018-11-27 18:48:14 +0100

    feat(controllers): Write category controllers, edit category model

--- router/index.js
+++ router/index.js
@@ -6,39 +6,39 @@ const itemsController = require('./../controllers/itemsController');
 // to authenticate user identity
 const authMiddleware = require('./../middlewares/authMiddleware');
 
-// (User) me controllers
-router
-  // get user info including liked categories
-  .get('/me', authMiddleware, usersController.getOwnInfo)
-  // include friends' liked categories?
-  .get('/me/friends', authMiddleware, usersController.getFriends)
-  .put(
-    '/me/categories/:category_id',
-    authMiddleware,
-    usersController.addCategory
-  )
-  .delete(
-    '/me/categories/:category_id',
-    authMiddleware,
-    usersController.removeCategory
-  );
-// .post('/me/friends', user.addFriend)
-// .delete('/me/friends', user.deleteFriend)
+// // (User) me controllers
+// router
+//   // get user info including liked categories
+//   .get('/me', authMiddleware, usersController.getOwnInfo)
+//   // include friends' liked categories?
+//   .get('/me/friends', authMiddleware, usersController.getFriends)
+//   .put(
+//     '/me/categories/:category_id',
+//     authMiddleware,
+//     usersController.addCategory
+//   )
+//   .delete(
+//     '/me/categories/:category_id',
+//     authMiddleware,
+//     usersController.removeCategory
+//   );
+// // .post('/me/friends', user.addFriend)
+// // .delete('/me/friends', user.deleteFriend)
 
-// User controllers
-router
-  .post('/register', usersController.createUser)
-  .get('/users/:user_id/items', authMiddleware, usersController.getLikedItems);
+// // User controllers
+// router
+//   .post('/register', usersController.createUser)
+//   .get('/users/:user_id/items', authMiddleware, usersController.getLikedItems);
 
-// Item controllers
-router
-  // get the array of recommended items for authenticated user
-  .get('/items/recommended', authMiddleware, itemsController.getRecommended)
-  .put(
-    '/items/:item_id/like/:value',
-    authMiddleware,
-    itemsController.setAffinity
-  );
+// // Item controllers
+// router
+//   // get the array of recommended items for authenticated user
+//   .get('/items/recommended', authMiddleware, itemsController.getRecommended)
+//   .put(
+//     '/items/:item_id/like/:value',
+//     authMiddleware,
+//     itemsController.setAffinity
+//   );
 
 // Category controllers
 router
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = express.json();
 const cors = require('cors');
 // const authMiddleware = require('authMiddlewareHere');
-// const router = require('./router');
+const router = require('./router');
 // add error handlers
 
 const app = express();
@@ -11,7 +11,7 @@ app
   .use(cors())
   .use(bodyParser)
   // .use(authMiddleware)
-  // .use(router)
+  .use(router)
   .use((req, res) => {
     res.status(404).send(`${req.path} not found!`);
   });
