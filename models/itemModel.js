097f09c6926ee7a0a815c701255d07d7a4f0fa92
rusolm <leandromarquesp88@gmail.com>
2018-11-27 12:24:06 +0100

    feat(items model): get all items from certain category

--- models/itemModel.js
+++ models/itemModel.js
@@ -10,4 +10,30 @@ itemModel.setItemsfromCategory = async bulk => {
   }
 };
 
+itemModel.getAllbyCagtegory = async catId => {
+  const items = await db.Item.findAll({
+    raw: true,
+    include: [
+      {
+        model: db.Category,
+        as: 'category',
+        where: {
+          category_id: catId
+        }
+      }
+    ]
+  });
+  console.log(items); // eslint-disable-line no-console
+};
+
+itemModel.getAllbyCagtegory(4);
+
+itemModel.getRecommended = async () => {};
+
+itemModel.setAffinity = async () => {};
+
+itemModel.removeAffinity = async () => {};
+
+// itemModel.getCategories();
+
 module.exports = itemModel;
