7b0fa28c8d51031dc7751017d76312c34c9e13a9
rusolm <leandromarquesp88@gmail.com>
2018-11-28 00:39:05 +0100

    feat(seed model): set categories (bulkcreate)

--- models/seedModel.js
+++ models/seedModel.js
@@ -3,11 +3,17 @@ const db = require('./../schemas');
 const seedModel = {};
 
 seedModel.setItemsfromCategory = async bulk => {
-  try {
-    await db.Item.bulkCreate(bulk);
-  } catch (error) {
-    console.log(error); // eslint-disable-line no-console
-  }
+  await db.Item.bulkCreate(bulk);
+};
+
+seedModel.setCategories = async bulk => {
+  await db.Category.bulkCreate(bulk);
 };
 
 module.exports = seedModel;
+
+/* 
+not able to bulkcreate with duplicates in postgres (today v.10.5)
+if migrate to mysql the following would work:
+db.Item.bulkCreate(bulk, { updateOnDuplicate: true })
+*/
