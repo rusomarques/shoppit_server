3b03417b9a43e61dd046b5f73a5d193c77421974
rusolm <leandromarquesp88@gmail.com>
2018-11-28 02:47:05 +0100

    feat(scrape controller): save items to db format (no duplicates)

--- scrape/scrape-controller.js
+++ scrape/scrape-controller.js
@@ -1,34 +1,40 @@
 const fs = require('fs');
+const _ = require('lodash');
 const data = require('./data/data.json');
 
 const scrapeController = {};
 
-scrapeController.seedCategories = async () => {
-  const categories = {
+scrapeController.seed = async () => {
+  const db = {
     categories: []
   };
+  const tempItems = [];
 
   try {
     let i = 0;
     for (let category in data) {
-      categories.categories.push({
+      db.categories.push({
         category_id: i,
         category_name: category
       });
       i++;
+
+      tempItems.push(...data[category]);
     }
+
+    db.items = _.uniqBy(tempItems, 'id');
   } catch (e) {
     console.log(e); // eslint-disable-line no-console
   }
 
   fs.writeFile(
-    __dirname + '/data/categories.json',
-    JSON.stringify(categories),
+    __dirname + '/data/seed-real-data.json',
+    JSON.stringify(db),
     'utf8',
     e => {
       /* eslint-disable no-console */
-      if (e) console.log(`couldn save to json`);
-      else console.log(`categories saved to /data/categories.json`);
+      if (e) console.log(`could not save to json`);
+      else console.log(`categories saved to /data/seed-real-data.json`);
     }
   );
 };
