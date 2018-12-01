577d25a3650bbd341913a0c88484b5214b9a0d65
Amy Kirasack <amykirasack@gmail.com>
2018-12-01 12:56:31 +0100

    fix(fetchAuth): Fix refactored fetch request

--- fetchAuth.js
+++ fetchAuth.js
@@ -1,9 +1,14 @@
 const rp = require('request-promise');
-const profileFields = require('./middleware/config').facebook;
+const { profileFields } = require('./middleware/config').facebook;
 
-exports.fetchFacebook = async accesstoken => {
+const fetchAuth = {};
+
+fetchAuth.facebook = async accesstoken => {
   const stringifiedProfile = await rp(
     `https://graph.facebook.com/me?access_token=${accesstoken}&fields=${profileFields}`
   );
+
   return JSON.parse(stringifiedProfile);
 };
+
+module.exports = fetchAuth;
