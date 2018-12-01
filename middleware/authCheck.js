957abd56e35553d92e3b77477451fb575c5eba19
Amy Kirasack <amykirasack@gmail.com>
2018-12-01 16:22:15 +0100

    refactor(create user): Modularize and extract react user controller

--- middleware/authCheck.js
+++ middleware/authCheck.js
@@ -1,37 +1,19 @@
 /* eslint-disable no-console */
 const db = require('../schemas');
 const fetchFacebook = require('../fetchAuth').facebook;
+const { upsertUser } = require('./../models/userModel');
 
 const authCheck = async (req, res, next) => {
   try {
     const { accesstoken } = req.headers;
-    // check in db for accessToken
     const currentUser = await db.User.findOne({
       where: { accesstoken }
     });
-
-    // // if exists in db call next with user id that matches accessToken
     if (currentUser) {
       next();
     } else {
-      // if doesn't exist query oAuth provider with accessToken
       const profile = await fetchFacebook(accesstoken);
-
-      // upsert user in db by user id returned from oAuth provider
-      if (profile.birthday) {
-        var birthdayDate = new Date(profile.birthday);
-      }
-      await db.User.upsert({
-        user_id: profile.id,
-        first_name: profile.first_name,
-        last_name: profile.last_name,
-        gender: profile.gender,
-        birthday: birthdayDate,
-        avatar_url: profile.picture.data.url,
-        email: profile.email,
-        accesstoken
-      });
-
+      await upsertUser(profile, accesstoken);
       next();
     }
   } catch (e) {
