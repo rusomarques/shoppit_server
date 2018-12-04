e85e1b1d7ba090894b5bd6108316c37f236b5310
rusolm <leandromarquesp88@gmail.com>
2018-12-04 01:30:33 +0100

    test(scrape):

--- __tests__/scrape.test.js
+++ __tests__/scrape.test.js
@@ -8,4 +8,21 @@ describe('Formating scrape to save into db', () => {
       formatedData.items.length
     );
   });
+  it('itemCategories length should be = to the sum of all categories length', async () => {
+    let sum = 0;
+    for (let category in mocks) {
+      sum += mocks[category].length;
+    }
+    const formatedData = await format.formatData(mocks);
+    expect(sum).toEqual(formatedData.itemCategories.length);
+  });
+  it('Categories length should be = to the number of categories', async () => {
+    let sum = 0;
+    for (let category in mocks) {
+      category;
+      sum++;
+    }
+    const formatedData = await format.formatData(mocks);
+    expect(sum).toEqual(formatedData.categories.length);
+  });
 });
