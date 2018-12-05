d63c7d4b50ef8ba63641f6eecc2b0442b0bf30d1
rusolm <leandromarquesp88@gmail.com>
2018-12-05 10:59:25 +0100

    refactor(naming): change category names

--- scrape/format-data.js
+++ scrape/format-data.js
@@ -37,8 +37,8 @@ scrapeController.formatData = async data => {
 };
 
 const renameCategory = name => {
-  if (name === 'for-her') return 'for her';
-  else if (name === 'for-him') return 'for him';
+  if (name === 'for-her') return 'a woman';
+  else if (name === 'for-him') return 'a man';
   else if (name === 'for-mom') return 'a mom';
   else if (name === 'for-dad') return 'a dad';
   else if (name === 'for-kids') return 'a kid at heart';
